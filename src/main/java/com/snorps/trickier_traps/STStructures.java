f4ec12c725cd1f6f651360d84bc9b0cd6304abcc
unknown <virtualistik@hotmail.co.uk>
2020-10-30 17:14:03 +0000

    tmessage

---
+++ src/main/java/com/snorps/trickier_traps/STStructures.java
@@ -0,0 +1,112 @@
+package com.snorps.trickier_traps;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.snorps.trickier_traps.structures.DesertPyramidStructure;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.settings.DimensionStructuresSettings;
+import net.minecraft.world.gen.settings.StructureSeparationSettings;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.function.Supplier;
+
+public class STStructures {
+
+    /**
+     * We are using the Deferred Registry system to register our structure as this is the preferred way on Forge.
+     * This will handle registering the base structure for us at the correct time so we don't have to handle it ourselves.
+     *
+     * HOWEVER, do note that Deferred Registries only work for anything that is a Forge Registry. This means that
+     * configured structures and configured features need to be registered directly to WorldGenRegistries as there
+     * is no Deferred Registry system for them.
+     */
+    public static final DeferredRegister<Structure<?>> DEFERRED_REGISTRY_STRUCTURE = DeferredRegister.create(ForgeRegistries.STRUCTURE_FEATURES, StructureTutorialMain.MODID);
+
+    /**
+     * Registers the structure itself and sets what its path is. In this case, the
+     * structure will have the resourcelocation of trickier_traps:desert_pyramid.
+     *
+     * It is always a good idea to register your Structures so that other mods and datapacks can
+     * use them too directly from the registries. It great for mod/datapacks compatibility.
+     *
+     * IMPORTANT: Once you have set the name for your structure below and distributed your mod,
+     * it should NEVER be changed or else it can cause worlds to become corrupted if they generated
+     * any chunks with your mod with the old structure name. See MC-194811 in Mojang's bug tracker for details.
+     *
+     * Forge has an issue report here: https://github.com/MinecraftForge/MinecraftForge/issues/7363
+     * Keep watch on that to know when it is safe to remove or change structure's registry names
+     */
+    public static final RegistryObject<Structure<NoFeatureConfig>> DESERT_PYRAMID = setupStructure("desert_pyramid", () -> (new DesertPyramidStructure(NoFeatureConfig.field_236558_a_)));
+
+    /**
+     * Helper method for registering all structures
+     */
+    private static <T extends Structure<?>> RegistryObject<T> setupStructure(String name, Supplier<T> structure) {
+        return DEFERRED_REGISTRY_STRUCTURE.register(name, structure);
+    }
+
+    /**
+     * This is where we set the rarity of your structures and determine if land conforms to it.
+     * See the comments in below for more details.
+     */
+    public static void setupStructures() {
+        setupStructure(
+                DESERT_PYRAMID.get(), /* The instance of the structure */
+                //spacing temporarily changed to 4,0 for playtesting, default is 30,0
+                new StructureSeparationSettings(4 /* maximum distance apart in chunks between spawn attempts */,
+                        0 /* minimum distance apart in chunks between spawn attempts */,
+                        1038494734 /* this modifies the seed of the structure so no two structures always spawn over each-other. Make this large and unique. */),
+                false);
+
+        // Add more structures here and so on
+    }
+
+    /**
+     * Adds the provided structure to the registry, and adds the separation settings.
+     * The rarity of the structure is determined based on the values passed into
+     * this method in the structureSeparationSettings argument. Called by registerFeatures.
+     */
+    public static <F extends Structure<?>> void setupStructure(
+            F structure,
+            StructureSeparationSettings structureSeparationSettings,
+            boolean transformSurroundingLand)
+    {
+        /*
+         * We need to add our structures into the map in Structure alongside vanilla
+         * structures or else it will cause errors. Called by registerStructure.
+         *
+         * If the registration is setup properly for the structure,
+         * getRegistryName() should never return null.
+         */
+        Structure.field_236365_a_.put(structure.getRegistryName().toString(), structure);
+
+        /*
+         * Will add land at the base of the structure like it does for Villages and Outposts.
+         * Doesn't work well on structure that have pieces stacked vertically or change in heights.
+         */
+        if(transformSurroundingLand){
+            Structure.field_236384_t_ =
+                    ImmutableList.<Structure<?>>builder()
+                            .addAll(Structure.field_236384_t_)
+                            .add(structure)
+                            .build();
+        }
+
+        /*
+         * Adds the structure's spacing into several places so that the structure's spacing remains
+         * correct in any dimension or worldtype instead of not spawning.
+         *
+         * However, it seems it doesn't always work for code made dimensions as they read from
+         * this list beforehand. Use the WorldEvent.Load event in StructureTutorialMain to add
+         * the structure spacing from this list into that dimension.
+         */
+        DimensionStructuresSettings.field_236191_b_ =
+                ImmutableMap.<Structure<?>, StructureSeparationSettings>builder()
+                        .putAll(DimensionStructuresSettings.field_236191_b_)
+                        .put(structure, structureSeparationSettings)
+                        .build();
+    }
+}
---
+++ src/main/java/com/snorps/trickier_traps/structures/DesertPyramidStructure.java
@@ -0,0 +1,196 @@
+package com.snorps.trickier_traps.structures;
+
+import com.google.common.collect.ImmutableList;
+import com.mojang.serialization.Codec;
+import com.snorps.trickier_traps.StructureTutorialMain;
+import net.minecraft.entity.EntityType;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SharedSeedRandom;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.util.registry.DynamicRegistries;
+import net.minecraft.util.registry.Registry;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.MobSpawnInfo;
+import net.minecraft.world.biome.provider.BiomeProvider;
+import net.minecraft.world.gen.ChunkGenerator;
+import net.minecraft.world.gen.GenerationStage;
+import net.minecraft.world.gen.Heightmap;
+import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraft.world.gen.feature.jigsaw.JigsawManager;
+import net.minecraft.world.gen.feature.structure.AbstractVillagePiece;
+import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.feature.structure.StructureStart;
+import net.minecraft.world.gen.feature.structure.VillageConfig;
+import net.minecraft.world.gen.feature.template.TemplateManager;
+import org.apache.logging.log4j.Level;
+
+import java.util.List;
+
+public class DesertPyramidStructure extends Structure<NoFeatureConfig> {
+    public DesertPyramidStructure(Codec<NoFeatureConfig> codec) {
+        super(codec);
+    }
+
+    /**
+     * This is how the worldgen code knows what to call when it
+     * is time to create the pieces of the structure for generation.
+     */
+    @Override
+    public  IStartFactory<NoFeatureConfig> getStartFactory() {
+        return DesertPyramidStructure.Start::new;
+    }
+
+
+    /**
+     * Generation stage for when to generate the structure. there are 10 stages you can pick from!
+     * This surface structure stage places the structure before plants and ores are generated.
+     */
+    @Override
+    public GenerationStage.Decoration func_236396_f_()
+    {
+        return GenerationStage.Decoration.SURFACE_STRUCTURES;
+    }
+
+
+    /**
+     * || ONLY WORKS IN FORGE 34.1.12+ ||
+     *
+     * This method allows us to have mobs that spawn naturally over time in our structure.
+     * No other mobs will spawn in the structure of the same entity classification.
+     * The reason you want to match the classifications is so that your structure's mob
+     * will contribute to that classification's cap. Otherwise, it may cause a runaway
+     * spawning of the mob that will never stop.
+     *
+     * NOTE: getDefaultSpawnList is for monsters only and getDefaultCreatureSpawnList is
+     *       for creatures only. If you want to add entities of another classification,
+     *       use the StructureSpawnListGatherEvent to add water_creatures, water_ambient,
+     *       ambient, or misc mobs. Use that event to add/remove mobs from structures
+     *       that are not your own.
+     */
+    private static final List<MobSpawnInfo.Spawners> STRUCTURE_MONSTERS = ImmutableList.of(
+            //new MobSpawnInfo.Spawners(EntityType.ILLUSIONER, 100, 4, 9),
+            //new MobSpawnInfo.Spawners(EntityType.VINDICATOR, 100, 4, 9)
+    );
+    @Override
+    public List<MobSpawnInfo.Spawners> getDefaultSpawnList() {
+        return STRUCTURE_MONSTERS;
+    }
+
+    private static final List<MobSpawnInfo.Spawners> STRUCTURE_CREATURES = ImmutableList.of(
+            //new MobSpawnInfo.Spawners(EntityType.SHEEP, 30, 10, 15),
+            //new MobSpawnInfo.Spawners(EntityType.RABBIT, 100, 1, 2)
+    );
+    @Override
+    public List<MobSpawnInfo.Spawners> getDefaultCreatureSpawnList() {
+        return STRUCTURE_CREATURES;
+    }
+
+
+    /*
+     * This is where extra checks can be done to determine if the structure can spawn here.
+     * This only needs to be overridden if you're adding additional spawn conditions.
+     * 
+     * Notice how the biome is also passed in. Though, you are not going to do any biome
+     * checking here as you should've added this structure to the biomes you
+     * wanted already with the biome load event.
+     * 
+     * Basically, this method is used for determining if the land is at a suitable height,
+     * if certain other structures are too close or not, or some other restrictive condition.
+     *
+     * For example, Pillager Outposts added a check to make sure it cannot spawn within 10 chunk of a Village.
+     * (Bedrock Edition seems to not have the same check)
+     * 
+     * 
+     * Also, please for the love of god, do not do dimension checking here. If you do and
+     * another mod's dimension is trying to spawn your structure, the locate
+     * command will make minecraft hang forever and break the game.
+     *
+     * Instead, use the WorldEvent.Load event in StructureTutorialMain class.
+     * If you check for the dimension there and do not add your structure's
+     * spacing into the chunk generator, the structure will not spawn in that dimension!
+     */
+    @Override
+    protected boolean func_230363_a_(ChunkGenerator chunkGenerator, BiomeProvider biomeSource, long seed, SharedSeedRandom chunkRandom, int chunkX, int chunkZ, Biome biome, ChunkPos chunkPos, NoFeatureConfig featureConfig) {
+        /*
+        int landHeight = chunkGenerator.getNoiseHeight(chunkX << 4, chunkZ << 4, Heightmap.Type.WORLD_SURFACE_WG);
+        boolean correctHeight = false;
+        if (landHeight == 64) {
+            correctHeight = true;
+        }
+        return correctHeight;
+        */
+
+        int landHeight = chunkGenerator.getNoiseHeight(chunkX << 4, chunkZ << 4, Heightmap.Type.WORLD_SURFACE_WG);
+
+        if (landHeight > 71) {
+            return false;
+        } else
+        {
+            return true;
+        }
+
+    }
+
+
+    /**
+     * Handles calling up the structure's pieces class and height that structure will spawn at.
+     */
+    public static class Start extends StructureStart<NoFeatureConfig>  {
+        public Start(Structure<NoFeatureConfig> structureIn, int chunkX, int chunkZ, MutableBoundingBox mutableBoundingBox, int referenceIn, long seedIn) {
+            super(structureIn, chunkX, chunkZ, mutableBoundingBox, referenceIn, seedIn);
+        }
+
+        @Override
+        public void func_230364_a_(DynamicRegistries dynamicRegistryManager, ChunkGenerator chunkGenerator, TemplateManager templateManagerIn, int chunkX, int chunkZ, Biome biomeIn, NoFeatureConfig config) {
+
+            // Turns the chunk coordinates into actual coordinates we can use. (Gets center of that chunk)
+            int x = (chunkX << 4) + 7;
+            int z = (chunkZ << 4) + 7;
+            BlockPos blockpos = new BlockPos(x, 64, z);
+
+            // All a structure has to do is call this method to turn it into a jigsaw based structure!
+            JigsawManager.func_242837_a(
+                    dynamicRegistryManager,
+                    new VillageConfig(() -> dynamicRegistryManager.getRegistry(Registry.JIGSAW_POOL_KEY)
+                            // The path to the starting Template Pool JSON file to read.
+                            .getOrDefault(new ResourceLocation(StructureTutorialMain.MODID, "desert_pyramid/desert_pyramid_starter")),
+
+                            // How many pieces outward from center can a recursive jigsaw structure spawn.
+                            // Our structure is only 1 block out and isn't recursive so any value of 1 or more doesn't change anything.
+                            // However, I recommend you keep this a high value so people can use datapacks to add additional pieces to your structure easily.
+                            50),
+                    AbstractVillagePiece::new,
+                    chunkGenerator,
+                    templateManagerIn,
+                    blockpos, // Position of the structure. Y value is ignored if last parameter is set to true.
+                    this.components, // The list that will be populated with the jigsaw pieces after this method.
+                    this.rand,
+                    true, // Allow intersecting jigsaw pieces. If false, villages cannot generate houses. I recommend to keep this to true.
+                    false); // Place at heightmap (top land). Set this to false for structure to be place at blockpos's y value instead
+
+
+            // Right here, you can do interesting stuff with the pieces in this.components such as offset the
+            // center piece by 50 blocks up for no reason, remove repeats of a piece or add a new piece so
+            // only 1 of that piece exists, etc. But you do not have access to the piece's blocks as this list
+            // holds just the piece's size and positions. Blocks will be placed later in JigsawManager.
+            //
+            // In this case, we offset the pieces up 1 so that the doorstep is not lower than the original
+            // terrain and then we extend the bounding box down by 1 to force down the land by 1 block that the
+            // Structure.field_236384_t_ field will place at bottom of the house. By lifting the house up by 1 and
+            // lowering the bounding box, the land at bottom of house will now stay in place instead of also being
+            // raise by 1 block because the land is based on the bounding box itself.
+            this.components.forEach(piece -> piece.offset(0, 1, 0));
+            this.components.forEach(piece -> piece.getBoundingBox().minY -= 1);
+
+
+            // Sets the bounds of the structure once you are finished.
+            this.recalculateStructureSize();
+
+            // I use to debug and quickly find out if the structure is spawning or not and where it is.
+            StructureTutorialMain.LOGGER.log(Level.DEBUG, "Desert Pyramid at " + (blockpos.getX()) + " " + blockpos.getY() + " " + (blockpos.getZ()));
+        }
+
+    }
+}
